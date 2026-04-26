0fa09ea111962b91f3a1a45ed4d1586af6bb8195
LevelHugyecz <hugyeczlevente12@gmail.com>
2026-04-26 21:47:33 +0200

    teszt fix

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+  function check() {
+    let s = 0;
 
+    document.querySelectorAll('input[type=radio]:checked')
+        .forEach(x => {
+            if (x.value === 'true') s++;
+        });
+
+    document.getElementById('res').innerText = 'Eredmény: ' + s + '/5';}
